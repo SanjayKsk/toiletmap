e553db3125ff99904a3fa58ddde782cf615fee9f
SanjayKsk <skhaira98@gmail.com>
2022-09-03 22:00:40 +0800

    Add toilet form functionality

--- src/components/toiletForm.tsx
+++ src/components/toiletForm.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
-// import Link from "next/link";
+import Link from "next/link";
 // import { Image } from "cloudinary-react";
 import { SearchBox } from "./searchBox";
 // import {
@@ -29,6 +29,7 @@ interface IProps {}
 
 export default function ToiletForm({}: IProps){
     const [submitting, setSubmitting] = useState(false);
+    const [previewImage, setPreviewImage] = useState<string>();
     const {register, handleSubmit, setValue, errors, watch} = useForm<IFormData>({ defaultValues: {}});
 
     const address = watch("address");
@@ -41,7 +42,7 @@ export default function ToiletForm({}: IProps){
     }, [register])
 
     const handleCreate = async(data: IFormData) => {
-
+        console.log({data});
     };
 
     const onSubmit = (data: IFormData) => {
@@ -61,6 +62,85 @@ export default function ToiletForm({}: IProps){
                 setValue("longitude", longitude);
             }}
             defaultValue=""/>
+        </div>
+
+        <div className="mt-4">
+            <label htmlFor="image" className="p-4 border-dashed border-4 border-gray-300 block cursor-pointer">
+                Click to add image 16:9
+            </label>
+            <input 
+                id="image" 
+                name="image" 
+                type="file" 
+                // Prevent users from selecting other types of files
+                accept="image/*" 
+                style={{ display: "none"}}
+                ref={register({
+                    validate: (fileList: FileList) => {
+                        if(fileList.length === 1) return true;
+                        return "Please select an image";
+                    }
+                })}
+
+                // Function to preview the image below the input box
+                onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    if (event?.target?.files?.[0]) {
+                        const file = event.target.files[0];
+                        const reader = new FileReader();
+                        reader.onloadend = () => {
+                            setPreviewImage(reader.result as string);
+                        };
+                        reader.readAsDataURL(file);
+                    }
+                }}
+            />
+            {previewImage && (
+                <img 
+                src={previewImage} 
+                className="mt-4 object-cover" 
+                style={{ width: "576px", height: `${(9/16) * 576}px`}}
+                />
+            )}
+            {errors.image && <p>{errors.image.message}</p>}
+        </div>
+        <div className="mt-4">
+            <label htmlFor="rating" className="block">Rating</label>
+            <input 
+                id="rating" 
+                name="rating" 
+                type="number" 
+                min ="0"
+                max="5"
+                step={0.5}
+                className="p-2" 
+                ref={register({
+                    required: "Please enter a rating",
+                    min: {value: 0, message: "Rating must be between 0 and 5"},
+                    max: {value: 5, message: "Rating must be between 0 and 5"},
+                    }
+                )} />
+                {errors.rating && <p>{errors.rating.message}</p>}
+        </div>
+        <div className="flex space-x-4">
+            <div className="mt-4 w-1/2">
+                <label htmlFor="handicapped" className="block">Handicapped Accessible?</label>
+                <input id="handicapped" name="handicapped" type="checkbox" className="p-2" ref={register} />
+            </div>
+
+            <div className="mt-4 w-1/2">
+                <label htmlFor="familyRoom" className="block">Baby changing facilities available?</label>
+                <input id="familyRoom" name="familyRoom" type="checkbox" className="p-2" ref={register} />
+            </div>
+        </div>
+
+        <div className="mt-14">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" disabled={submitting}>
+                Add Toilet
+            </button>{" "}
+
+            <Link href="/">
+                Cancel
+            </Link>
         </div>
     </form>
     );
